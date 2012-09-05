92b77f854774a5912774246894276a953efc7f49
Erlend Oftedal <erlend@oftedal.no>
2012-09-05 09:56:39 +0200

    Fix assets js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Csptesting::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile << '*.js'
 
   # Generate digests for assets URLs
   config.assets.digest = true
