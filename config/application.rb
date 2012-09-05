7f5d0c2fa9f5cf5bd9ac54721b027bcc00ab91c3
Erlend Oftedal <erlend@oftedal.no>
2012-09-05 09:38:17 +0200

    Fixing sessions

--- config/application.rb
+++ config/application.rb
@@ -3,8 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 
-require 'config/memory_store'
-
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
