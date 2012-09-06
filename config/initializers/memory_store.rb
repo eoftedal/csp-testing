27a9a0a33654d8b88774edc0078cbdde07db4d89
Erlend Oftedal <erlend@oftedal.no>
2012-09-06 22:27:12 +0200

    Fixing session id

--- config/initializers/memory_store.rb
+++ config/initializers/memory_store.rb
@@ -6,15 +6,18 @@ module ActionDispatch
 
       private
         def get_session(env, sid)
-          sid ||= generate_sid
-          session = GLOBAL_HASH_TABLE[sid] || {}
+          sid ||= SecureRandom.hex(16)
+          session = GLOBAL_HASH_TABLE[sid] || { :created => Time.now() }
+          if (session[:created] == nil || session[:created] < (Time.now() - 30*60)) 
+            session = { :created => Time.now() }
+          end
           session = Rack::Session::Abstract::SessionHash.new(self, env).merge(session)
           [sid, session]
         end
 
         def set_session(env, sid, session_data, cookie_settings)
           GLOBAL_HASH_TABLE[sid] = session_data
-          return true
+          return sid
         end
     end
   end
